2766de6aa9a0b28d3d8b59ea8efc11fece38f30a
JohnWickpaul <107850797+JohnWickpaul@users.noreply.github.com>
2023-10-27 14:38:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['919074692450', 'ABHISHEK-SURESH', true],
+  ['263718329500', 'SoftwareJohnWick-PaulX', true],
   [''],
   [''] 
 ] //Number of owners
